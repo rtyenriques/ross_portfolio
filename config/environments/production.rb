32d9893e57c541ae89dc829bf18419e89ce651bc
= <rtyenriques@gmail.com>
2022-10-17 00:08:07 -0700

    changed port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,5 +118,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 5000}
 end
